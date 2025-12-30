17ed9870723af8f1ff02f089fba7f8c31c814758
Om <om.kumar@manprax.com>
2025-12-30 19:21:18 +0530

    added beep sound when user click on speak btn

--- dist/AudioSearch.js
+++ dist/AudioSearch.js
@@ -751,11 +751,11 @@ class AudioSearch extends Component {
 
             // }
 
-            // setTimeout(() => {
-            //     if (this.state.isListening) {
-            //       this.playBeepSound();
-            //     }
-            //   }, 3000);
+            setTimeout(() => {
+              if (this.state.isListening) {
+                this.playBeepSound();
+              }
+            }, 3000);
           });
         } catch (error) {
           console.error('Error starting MediaRecorder or SpeechRecognition:', {
